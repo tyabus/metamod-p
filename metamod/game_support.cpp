8c38700ade65cdea89997aa814a76b15314c1c26
tyabus <tyabustest@gmail.com>
2019-05-06 12:54:52 +0300

    If there are 2 or more same names check next dll file if doesn't exist

--- metamod/game_support.cpp
+++ metamod/game_support.cpp
@@ -62,11 +62,25 @@
 // Find a modinfo corresponding to the given game name.
 const game_modinfo_t * DLLINTERNAL lookup_game(const char *name) {
 	const game_modinfo_t *imod;
+	char check_path[NAME_MAX];
 	int i;
 	for(i=0; known_games[i].name; i++) {
 		imod=&known_games[i];
+		// If there are 2 or more same names check next dll file if doesn't exist
 		if(strcasematch(imod->name, name))
+			safevoid_snprintf(check_path, sizeof(check_path), "dlls/%s",
+#ifdef _WIN32
+					 imod->win_dll);
+#elif defined(linux)
+					 imod->linux_so);
+#endif
+
+			if(!valid_gamedir_file(check_path)) {
+				continue;
+			}
+
 			return(imod);
+		}
 	}
 	// no match found
 	return(NULL);
